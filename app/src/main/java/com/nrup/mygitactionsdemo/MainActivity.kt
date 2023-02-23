6295b386bb20cf37e7d0a720e2889a00813b78a5
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2023-02-23 17:47:03 +0530

    - Keystore added in cert folder - Flavor added debug/qa/release - git ignore updated both app level and project level - UI changes. Show Base URL and Build type in TextView

--- app/src/main/java/com/nrup/mygitactionsdemo/MainActivity.kt
+++ app/src/main/java/com/nrup/mygitactionsdemo/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.nrup.mygitactionsdemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nrup.mygitactionsdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
+        binding.tvBuildInfo.text = """
+                Build Type ${BuildConfig.BUILD_TYPE}
+                BASE_URL ${BuildConfig.BASE_URL}
+                
+            """.trimIndent()
 
     }
 }

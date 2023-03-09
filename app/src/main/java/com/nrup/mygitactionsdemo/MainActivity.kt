9f6dc402e62a0022ef7548c71cb6c06fcad5ff0d
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2023-03-09 12:01:08 +0530

    - Master YML Updated

--- app/src/main/java/com/nrup/mygitactionsdemo/MainActivity.kt
+++ app/src/main/java/com/nrup/mygitactionsdemo/MainActivity.kt
@@ -11,6 +11,7 @@ import com.nrup.mygitactionsdemo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

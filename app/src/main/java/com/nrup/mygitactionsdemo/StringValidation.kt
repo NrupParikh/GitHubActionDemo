11782e09aa6772091db345258e8de634b1f77d43
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2023-03-15 11:53:37 +0530

    - String Validation demo added

---
+++ app/src/main/java/com/nrup/mygitactionsdemo/StringValidation.kt
@@ -0,0 +1,49 @@
+package com.nrup.mygitactionsdemo
+
+fun main() {
+
+    println("Enter string to check")
+    val myString = readLine()!!.toString()
+
+    println("You have enter $myString to check")
+
+    val myObject = StringValidation()
+    println("Total validation failed are  ${myObject.checkValidation(myString)}")
+
+}
+
+/*
+    ^                       : the starting of the string
+    (?=.*[a-z])             : at least one lowercase character.
+    (?=.*[A-Z])             : at least one uppercase character
+    (?=.*\d)                : at least one digit
+    (?=.*[-+_!@#$%^&*.,?])  : at least one special character
+    .                       : any character except line break
+    +                       : one or more times
+
+* */
+
+class StringValidation {
+
+    var counter: Int = 0
+
+    fun checkValidation(myString: String): Int {
+
+        if (!myString.contains("[A-Z]".toRegex())) {
+            counter++
+        }
+        if (!myString.contains("[a-z]".toRegex())) {
+            counter++
+        }
+
+        if (!myString.contains("\\d".toRegex())) {
+            counter++
+        }
+
+        if (!myString.contains("[-+_!@#\$%^&*.,?]".toRegex())) {
+            counter++
+        }
+        return counter
+
+    }
+}
